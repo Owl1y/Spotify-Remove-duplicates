7acd273e55e8395a18041505935c927a3324ced6
aj <ali.becerra1224@gmail.com>
2023-02-06 19:57:40 -0800

    made this branch cause songs have the same ids for some reason

--- remove-repeated-songs.py
+++ remove-repeated-songs.py
@@ -20,6 +20,7 @@ playlist = client.playlists(user_id_cut)
 
 def get_song_ids(the_playlist):
     song_ids = []
+    song_names = []
     play_offset =0 
     
     playlist_songs = client.playlist_items(the_playlist, as_tracks=False, offset=play_offset)
@@ -28,11 +29,13 @@ def get_song_ids(the_playlist):
     while full_list != len(song_ids):
         for songs in playlist_songs.items:
             song_id = songs.track.uri
+            song_name = songs.track.name
             song_ids.append(song_id)
+            song_names.append(song_name)
             #print(song_id)
         play_offset+=100
         playlist_songs = client.playlist_items(the_playlist, as_tracks=False, offset=play_offset)
-    return song_ids
+    return song_ids, song_names
 
 former_playlist = ['7IHZvZIusTuJNMcKleSWwd', 
                    '6fYjLM7ESHGrh5N9SvzVGB', 
@@ -43,10 +46,18 @@ former_playlist = ['7IHZvZIusTuJNMcKleSWwd',
                    '0AFQ3bdD6zCyXda4FOwp39']
 current_playlist = ['5TMatpArr9xKImvUHGydS8']
 
-current_pl_uris = get_song_ids(current_playlist[0])
+current_pl_uris, current_pl_names = get_song_ids(current_playlist[0])
+
+
 for nums in former_playlist:
-    former_pl_uris = get_song_ids(nums)
+    former_pl_uris, former_song_name_pl = get_song_ids(nums)
+    # print(former_song_name_pl)
     duplicate_uris = [songs for songs in former_pl_uris if songs in current_pl_uris]
+    duplicate_name = [name for name in former_song_name_pl if name in current_pl_names]
     client.playlist_remove(current_playlist[0], duplicate_uris)
-    print(duplicate_uris)
+    # print(former_song_name_pl)
+    # print(duplicate_uris)
+    print(duplicate_name)
+    # print(duplicate_uris)
+
 
